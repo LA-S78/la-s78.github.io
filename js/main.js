3b537e116ae6b43116d434b3eff3844e0828eb5a
LA-S78 <lastasylums78@gmail.com>
2026-06-15 06:44:01 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 /* /js/main.js */
 const observerOptions = {
-    root: null,
-    threshold: 0.3
+    root: null, 
+    // This tells the observer: "Start detecting 200px down from the top"
+    // Adjust -200px to match your total Header + Nav height
+    rootMargin: '-200px 0px -50% 0px', 
+    threshold: 0
 };
 
 const observer = new IntersectionObserver((entries) => {
