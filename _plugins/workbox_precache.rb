99f66732ebd3146763b53a0b4cdb72588b4cd98b
Josh <N/A>
2026-07-03 00:55:03 +0100

    debug

--- _plugins/workbox_precache.rb
+++ _plugins/workbox_precache.rb
@@ -3,32 +3,27 @@ require 'json'
 
 module WorkboxInjector
   def self.run(dest_path)
-    # 1. Generate the master list of all files in _site
-    search_pattern = File.join(dest_path, '**', '*.{html,css,js,png,jpg,jpeg,svg,webp,webmanifest}')
-    files = Dir.glob(search_pattern)
+    # The only sw.js that matters is at the root
+    sw_path = File.join(dest_path, 'sw.js')
+    
+    # 1. Glob EVERYTHING in the _site folder
+    files = Dir.glob(File.join(dest_path, '**', '*.{html,css,js,png,jpg,jpeg,svg,webp,webmanifest}'))
     
     precache_list = files.filter_map do |file|
-      next if file.include?('sw.js') # Don't cache the SW itself
+      # Skip the SW itself
+      next if file == sw_path
+      
+      # Create the absolute URL (e.g., /en/guides/duel.html)
       url = file.sub(dest_path, '')
       revision = Digest::MD5.file(file).hexdigest[0, 10]
       { 'url' => url, 'revision' => revision }
     end
     
+    # 2. Inject into the ROOT sw.js
+    sw_content = File.read(sw_path)
     injection = "const precacheList = #{precache_list.to_json};\n\n"
+    File.write(sw_path, injection + sw_content)
     
-    # 2. Find and inject into EVERY sw.js file found
-    sw_files = Dir.glob(File.join(dest_path, '**', 'sw.js'))
-    
-    sw_files.each do |sw_path|
-      puts "Injecting into: #{sw_path}"
-      sw_content = File.read(sw_path)
-      
-      # Only inject if it hasn't been injected yet to prevent duplicate headers
-      unless sw_content.include?('const precacheList')
-        File.write(sw_path, injection + sw_content)
-      end
-    end
-    
-    puts "Successfully injected #{precache_list.length} files into #{sw_files.length} Service Worker files!"
+    puts "Successfully injected #{precache_list.length} files into root sw.js!"
   end
 end
