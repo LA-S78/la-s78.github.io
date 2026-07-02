327a2770bc84a928cb2c1d2c4fb2e24d47174f1f
Josh <N/A>
2026-07-03 00:10:08 +0100

    workflow changes to incorporate caching

--- _plugins/workbox_precache.rb
+++ _plugins/workbox_precache.rb
@@ -1,41 +1,26 @@
 require 'digest'
 require 'json'
 
-Jekyll::Hooks.register :site, :post_write do |site|
-  Jekyll.logger.info "Workbox:", "Generating offline precache list..."
-  
-  sw_path = File.join(site.dest, 'sw.js')
-  
-  # Ensure Jekyll actually generated the Service Worker before trying to modify it
-  if File.exist?(sw_path)
-    # 1. Glob all the assets and localized guides we want to cache
-    search_pattern = File.join(site.dest, '**', '*.{html,css,js,png,jpg,jpeg,svg,webp,webmanifest}')
+module WorkboxInjector
+  def self.run(dest_path)
+    sw_path = File.join(dest_path, 'sw.js')
+    
+    # 1. Glob all assets and all language subdirectories
+    search_pattern = File.join(dest_path, '**', '*.{html,css,js,png,jpg,jpeg,svg,webp,webmanifest}')
     files = Dir.glob(search_pattern)
     
     precache_list = files.filter_map do |file|
-      # Do not cache the Service Worker itself to prevent update loops
-      next if file == sw_path
-      
-      # Strip the '_site' prefix to create the absolute URL path
-      url = file.sub(site.dest, '')
-      
-      # Generate an MD5 hash of the file contents to tell Workbox when a file updates
+      next if file.include?('sw.js') # Avoid caching the SW itself
+      url = file.sub(dest_path, '')
       revision = Digest::MD5.file(file).hexdigest[0, 10]
-      
       { 'url' => url, 'revision' => revision }
     end
     
-    # 2. Read the newly compiled sw.js file
-    sw_content = File.read(sw_path)
-    
-    # 3. Create the exact JavaScript variable your sw.js is expecting
+    # 2. Read, Inject, and Write
+    sw_content = File.read('sw.js') # Read source sw.js
     injection = "const precacheList = #{precache_list.to_json};\n\n"
-    
-    # 4. Overwrite sw.js with the injected variable securely at the top
     File.write(sw_path, injection + sw_content)
     
-    Jekyll.logger.info "Workbox:", "Successfully encrypted and injected #{precache_list.length} files into sw.js!"
-  else
-    Jekyll.logger.warn "Workbox:", "sw.js not found in _site folder. Skipping precache."
+    puts "Successfully injected #{precache_list.length} files!"
   end
 end
